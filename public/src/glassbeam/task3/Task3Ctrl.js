bb2c99385e5f9ac8ee1648b66abac3a59c1aba20
Gulfaraz Yasin <gulfarazyasin@gmail.com>
2015-06-16 18:52:14 +0000

    task 3 - dereferenced data_set in sessionStorage, task 1 - increased pagination width

--- public/src/glassbeam/task3/Task3Ctrl.js
+++ public/src/glassbeam/task3/Task3Ctrl.js
@@ -8,23 +8,24 @@ angular.module('glassbeamApp.task3', ['ui.router', 'ngStorage'])
                 'controller' : 'task3Controller'
             });
     }])
-    .controller('task3Controller', ['$scope', '$sessionStorage', function ($scope, $sessionStorage) {
+    .controller('task3Controller', ['$scope', '$sessionStorage', '$state', function ($scope, $sessionStorage, $state) {
         $scope.data_set = [
             { 'name' : 'Gulfaraz', 'description': 'Awesome Guy' },
             { 'name' : 'Thamina', 'description': 'Awesome Gal' },
             { 'name' : 'Mohsin', 'description': 'Not so awesome Guy' }
         ];
         if($sessionStorage.data_set && $sessionStorage.data_set.length > 0) {
-            $scope.data_set = $sessionStorage.data_set;
+            $scope.data_set = angular.copy($sessionStorage.data_set);
         }
         $scope.save = function () {
-            $sessionStorage.data_set = $scope.data_set = $scope.data_set.filter(function (item) {
+            $sessionStorage.data_set = angular.copy($scope.data_set.filter(function (item) {
                 if(!item.delete) {
                     delete item.delete;
                     return true;
                 } else {
                     return false;
                 }
-            });
+            }));
+	    $state.go('task3', {}, { 'reload' : true });
         };
     }]);
